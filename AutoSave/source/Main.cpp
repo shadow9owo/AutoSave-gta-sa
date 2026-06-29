d1ef1900b87694819a598271b1037f9f421d9662
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2026-06-29 08:21:51 +0200

    Add files via upload

---
+++ AutoSave/source/Main.cpp
@@ -0,0 +1,40 @@
+#include <plugin.h> // Plugin-SDK version 1004 from 2026-04-18 13:03:53
+#include <CMessages.h>
+#include <CPedGroups.h>
+#include <CGenericGameStorage.h>
+
+using namespace plugin;
+
+struct Main
+{
+    bool WasOnMission;
+
+    Main()
+    {
+        // register event callbacks
+        Events::processScriptsEvent += []{ gInstance.OnGameProcess(); };
+        Events::initGameEvent += [] {gInstance.Init(); };
+    }
+
+    void Init()
+    {
+        WasOnMission = CPedGroups::ms_bIsPlayerOnAMission;
+        return;
+    }
+
+    void OnGameProcess()
+    {
+        if (WasOnMission && !CPedGroups::ms_bIsPlayerOnAMission)
+        {
+            if (CGenericGameStorage::GenericSave(1)) //i have no clue about the meaning of this param maybe some docs are in order?
+            {
+                CMessages::AddMessageJumpQ("Autosaving...", 500, 0);
+            }
+            else {
+                CMessages::AddMessageJumpQ("Failed to autosave...", 500, 0);
+            }
+        }
+
+        WasOnMission = CPedGroups::ms_bIsPlayerOnAMission;
+    }
+} gInstance;
